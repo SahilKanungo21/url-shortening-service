7477dfd0cade88bb1948ba221ab9a78260a920b9
Sahil Kanungo <kanungosahil123@gmail.com>
2023-01-08 20:57:33 +0530

    Initial commit

---
+++ src/test/java/com/system/urlshorteningservice/UrlShorteningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.urlshorteningservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShorteningServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
